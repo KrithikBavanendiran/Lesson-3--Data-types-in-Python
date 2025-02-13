f6ba680a45ff59cc33dfde4f05d79fc63b54dccc
KrithikBavanendiran <bvino1522@gmail.com>
2025-02-13 18:35:57 +0000

    Typecasting

---
+++ Typecasting.py
@@ -0,0 +1,12 @@
+String="Krithik"
+Boolean=True
+Integer=24
+Float=13.23
+print(type(String))
+print(type(Boolean))
+print(type(Integer))
+print(type(Float))
+Float=int(Float)
+print(type(Float))
+Integer=str(Integer)
+print(type(Integer))
